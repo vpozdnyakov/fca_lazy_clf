e3603ac3cc788ca30178367b6c9f9d6cf51ca52c
Виталий Поздняков <vitalijpozdnakov@MacBook-Pro-Vitalij-2.local>
2019-12-30 18:31:43 +0300

    update

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
-from distutils.core import setup
-setup(
-  name = 'fca_lazy_clf',
-  packages = ['fca_lazy_clf'],
-  version = '0.1',
-  license = 'MIT',
-  description = 'Lazy binary classifier based on Formal Concept Analysis',
-  author = 'Vitaliy Pozdnyakov',
-  author_email = 'pozdnyakov.vitaliy@yandex.ru',
-  url = 'https://github.com/vpozdnyakov/fca_lazy_clf',
-  download_url = 'https://github.com/vpozdnyakov/fca_lazy_clf/archive/0.1.tar.gz',
-  keywords = ['fca', 'formal-concept-analysis', 'lazy-learning', 'binary-classification'],
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name='fca_lazy_clf',
+  packages=['fca_lazy_clf'],
+  version='0.2',
+  license='MIT',
+  description='Lazy binary classifier based on Formal Concept Analysis',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author='Vitaliy Pozdnyakov',
+  author_email='pozdnyakov.vitaliy@yandex.ru',
+  url='https://github.com/vpozdnyakov/fca_lazy_clf',
+  download_url='https://github.com/vpozdnyakov/fca_lazy_clf/archive/0.2.tar.gz',
+  keywords=['fca', 'formal-concept-analysis', 'lazy-learning', 'binary-classification'],
   install_requires=[
           'pandas',
           'numpy',
@@ -18,7 +24,6 @@ setup(
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
   ],
